db69a0a788e1795c49ecaece03f7bd431eecf5fd
ssivaram560-glitch <ssivaram560@gmail.com>
2026-08-06 16:58:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const puppeteer   = require('puppeteer');
 // ============================================================
 //  CONFIG
 // ============================================================
-const BOT_TOKEN    ="8906099266:AAGgGfjc4OcJE_tdG32R_ylKGp4x699opek";
+const BOT_TOKEN    ="8999335291:AAG513_SpPYv5KVYCsImzHYEubYpJWzl8yc";
 const OWNER_ID     = 1865939951;
 const OWNER_PASS   = "praveensaran";
 const ADMIN_HANDLE = "@lucifer1570";
